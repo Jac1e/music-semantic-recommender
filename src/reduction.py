8d651c30a1a96fa00769e889db2884622e5cb9a5
Angela Gu <angelagu0706@gmail.com>
2026-04-24 02:48:12 -0400

    Update reduction.py
    add PCA projection for song recommendation

--- src/reduction.py
+++ src/reduction.py
@@ -3,23 +3,38 @@ from sklearn.decomposition import PCA
 
 
 DEFAULT_FEATURE_COLS = [
-    "danceability",
-    "energy",
-    "key",
-    "loudness",
-    "mode",
-    "speechiness",
-    "acousticness",
-    "instrumentalness",
-    "liveness",
-    "valence",
-    "tempo",
+    "danceability_scaled",
+    "energy_scaled",
+    "loudness_scaled",
+    "speechiness_scaled",
+    "acousticness_scaled",
+    "instrumentalness_scaled",
+    "liveness_scaled",
+    "valence_scaled",
+    "tempo_scaled",
+    "explicit_scaled",
 ]
 
 
 def compute_pca_projection(df, feature_cols=None, n_components=2):
     """
-    Project high-dimensional song audio features into a lower-dimensional PCA space.
+    Project high-dimensional song feature vectors into a lower-dimensional PCA space.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        Input dataframe containing processed song features.
+    feature_cols : list of str, optional
+        Feature columns used for PCA. If None, the default scaled audio features are used.
+    n_components : int
+        Number of PCA components. Usually 2 or 3.
+
+    Returns
+    -------
+    projected_df : pandas.DataFrame
+        A copy of the input dataframe with PCA coordinate columns added.
+    explained_variance_ratio : numpy.ndarray
+        Variance explained by each PCA component.
     """
     if feature_cols is None:
         feature_cols = DEFAULT_FEATURE_COLS
